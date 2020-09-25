762731e14e561a58cb4d5071350bf5ae7faf8eca
asohamithran <ashokharty99@gmail.com>
2020-09-25 15:58:15 +0530

    actually implemented the algorithm

--- Image_fft.py
+++ Image_fft.py
@@ -18,21 +18,41 @@ Image_2= ImageOps.grayscale(image_1)
 matrix_2= np.asarray(Image_2)
 fft_matrix_2= np.fft.fft2(matrix_2)
 Ifft_matrix_2= np.fft.ifft2(fft_matrix_2)
-fft_matrix_2= 20*np.log(abs(np.fft.fftshift(fft_matrix_2))) # upscaled_FFT
+F= 20*np.log(abs(np.fft.fftshift(fft_matrix_2))) # upscaled_FFT
 #print(np.max(matrix_2))
-fft_image= Image.fromarray(fft_matrix_2)
+fft_image= Image.fromarray(F)
 fft_image.convert("RGB").save("Upscaled_fft.PNG") #convert the image to b/w to RGB channel
 Ifft_matrix_2=abs(Ifft_matrix_2)
 iFFT_image= Image.fromarray(Ifft_matrix_2)
 # iFFT_image.show()
 print("\nshowing the image in freq space and real space")
+def compress(fft_image_matrix,row,col):
+    threshold=0.001* 0.2*np.max(abs(fft_image_matrix))
+
+    count=0
+    for i in range(row):
+        for j in range(col):
+        ## get the element in each position : check if it is higher than threshold
+            if(abs(fft_image_matrix[i,j])>threshold):
+                count+=1
+            else:
+                fft_image_matrix[i,j]=0
+
+    return fft_image_matrix,count
+row,col = np.shape(fft_matrix_2)
+C,count=compress(fft_matrix_2,row,col)
+print(f"No of pixels kept ={count},out of {row*col}" )
+T=20*np.log(abs(np.fft.fftshift(C)))
+#compressed image transformation
+cmp_image=np.fft.ifft2(C)
+
 plt.figure()
 plt.subplot(2,2,1)
-plt.imshow(matrix1)
+plt.imshow(F)
 plt.subplot(2,2,2)
 plt.imshow(matrix_2,cmap="gray")
 plt.subplot(2,2,3)
-plt.imshow(fft_matrix_2)
+plt.imshow(T)
 plt.subplot(2,2,4)
-plt.imshow(Ifft_matrix_2,cmap="gray")
+plt.imshow(abs(cmp_image),cmap="gray")
 plt.show()
