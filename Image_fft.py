fb91ffe6400ddec1d24a7e1f917d125a9dbf4d18
asohamithran <ashokharty99@gmail.com>
2020-09-25 14:34:22 +0530

    correct code

--- Image_fft.py
+++ Image_fft.py
@@ -4,24 +4,6 @@ import matplotlib.image as mig
 from pathlib import Path
 import scipy
 from PIL import Image,ImageOps
-image_path = Path("Sambhabana Mohanty.jpg")
-## get the image and convert into a matrix
-image_1= Image.open(image_path)
-matrix1=np.asarray(image_1)
-## convert the rgb image to grayscale image
-Image_2= ImageOps.grayscale(image_1)
-#Image_2.show()
-matrix_2= np.asarray(Image_2)
-fft_matrix_2= np.fft.fft2(matrix_2)
-Ifft_matrix_2= np.fft.ifft2(fft_matrix_2)
-fft_matrix_2= np.log(abs(np.fft.fftshift(fft_matrix_2)))
-print(np.max(matrix_2))
-fft_image= Image.fromarray(fft_matrix_2)import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mig
-from pathlib import Path
-import scipy
-from PIL import Image,ImageOps
 
 
 image_path = Path("Sambhabana Mohanty.jpg")
@@ -54,7 +36,3 @@ plt.imshow(fft_matrix_2)
 plt.subplot(2,2,4)
 plt.imshow(Ifft_matrix_2,cmap="gray")
 plt.show()
-
-#fft_image.show()
-iFFT_image= Image.fromarray(abs(Ifft_matrix_2))
-iFFT_image.show()
